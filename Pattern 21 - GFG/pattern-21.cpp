0a62f1a4b82e34ec54aa4abfdfecaaf30b54c1f0
avniaggarwal09 <137319715+avniaggarwal09@users.noreply.github.com>
2023-07-09 22:44:23 +0530

    Added solution - LeetHub

--- Pattern 21 - GFG/pattern-21.cpp
+++ Pattern 21 - GFG/pattern-21.cpp
@@ -26,6 +26,21 @@ class Solution {
     }
 };
 
+/*
+for (int i=0; i<n; i++) {
+    for (int j=0; j<n; j++) {
+        if (i==0 || i==n-1 || j==0 || j==n-1) {
+            cout<<'*';
+        }
+        else {
+            cout<<' ';
+        }
+        cout<<endl;
+    }
+}
+*/
+
+
 //{ Driver Code Starts.
 
 int main() {
